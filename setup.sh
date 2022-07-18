f669fea18fc28d3aed38296985c504fd05270cd0
nam000termux <77914563+nam000termux@users.noreply.github.com>
2022-07-18 10:10:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
-apt update
+23540red='\033[1;31m' 2> /dev/null
+green='\033[1;32m' 2> /dev/null
+yellow='\033[1;33m' 2> /dev/null
+blue='\033[1;34m' 2> /dev/null
+light_cyan='\033[1;96m' 2> /dev/null
+reset='\033[0m' 2> /dev/null
 
 printf "${blue}######################################################################\n"
 printf "${blue}##                                                                  ##\n"
 printf "${blue}## 8888888b.  8888888888 888888b.  8888888        d8888 888b    888 ##\n"
-printf "${blue}## 888  "Y88b 888        888  "88b   888         d88888 8888b   888 ##\n"
+printf "${blue}## 888   Y88b 888        888   88b   888         d88888 8888b   888 ##\n"
 printf "${blue}## 888    888 888        888  .88P   888        d88P888 88888b  888 ##\n"
 printf "${blue}## 888    888 8888888    8888888K.   888       d88P 888 888Y88b 888 ##\n"
-printf "${blue}## 888    888 888        888  "Y88b  888      d88P  888 888 Y88b888 ##\n"
+printf "${blue}## 888    888 888        888   Y88b  888      d88P  888 888 Y88b888 ##\n"
 printf "${blue}## 888    888 888        888    888  888     d88P   888 888  Y88888 ##\n"
 printf "${blue}## 888  .d88P 888        888   d88P  888    d8888888888 888   Y8888 ##\n"
-printf "${blue}## 8888888P"  8888888888 8888888P" 8888888 d88P     888 888    Y888 ##\n"
+printf "${blue}## 8888888P   8888888888 8888888P  8888888 d88P     888 888    Y888 ##\n"
 printf "${blue}##                                                                  ##\n"
 printf "${blue}##                                      Full proot-distro by Creeda ##\n"
 printf "${blue}######################################################################${reset}\n\n"
