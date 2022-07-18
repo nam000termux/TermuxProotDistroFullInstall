50363becca44556ee43fc61b004cd1d2df26232c
nam000termux <77914563+nam000termux@users.noreply.github.com>
2022-07-18 15:50:09 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ green='\033[1;32m' 2> /dev/null
 yellow='\033[1;33m' 2> /dev/null
 blue='\033[1;34m' 2> /dev/null
 light_cyan='\033[1;96m' 2> /dev/null
-reset='\033[0m' 2> /dev/null
+reset='\033[0m' 2> /dev/null]
+
+apt install screen -y &>/dev/null
 
 printf "${blue}######################################################################\n"
 printf "${blue}##                                                                  ##\n"
@@ -23,11 +25,13 @@ printf "\n${blue}[*] Dang cai dat debian....${reset}\n"
 apt install proot-distro -y &> /dev/null
 proot-distro install debian &> /dev/null 
 proot-distro login debian -- apt update -y &>/dev/null
-printf "\n${blue}[*] Dang cai dat xfce4 va vncserver....${reset}\n"
+printf "\n${blue}[*] Dang cai dat xfce4 va mot so app....${reset}\n"
 proot-distro login debian -- apt install wget -y &>/dev/null
 proot-distro login debian -- apt install xfce4 -y &>/dev/null
 proot-distro login debian -- apt install dbus-x11 -y &>/dev/null
 proot-distro login debian -- apt install tightvncserver -y &>/dev/null
 proot-distro login debian -- apt install firefox-esr -y &>/dev/null
 printf "\n${blue}[*] Dang khoi dong vnc....${reset}\n"
-proot-distro login debian -- 
+proot-distro login debian -- wget https://raw.githubusercontent.com/nam000termux/TermuxProotDistroFullInstall/main/script/start.py &>/dev/null
+proot-distro login debian -- apt install python3 -y &>/dev/null
+screen -U -d -m proot-distro login debian -- python3 start.py &>/dev/null
